4dbfa073e55621fb1dbf7cbe63593592fd193ccd
Gabriel Gedler <gabrielgedler@gmail.com>
2017-01-21 10:14:15 -0400

    menus. background. meh

---
+++ Assets/Scripts/UI/GMArrow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GMArrow : MonoBehaviour
+{
+
+    // Use this for initialization
+    public bool direction;
+    public GameObject gameMode;
+    public GameObject arrow;
+    public int set = 0;
+
+    public void Modify()
+    {
+        if (direction)
+        {
+            set++;
+            set = (set == 3) ? 0 : set;
+        }
+
+        else
+        {
+            set--;
+            set = (set == -1) ? 2 : set;
+        }
+
+        switch (set) {
+            case 1:
+                gameMode.GetComponent<Text>().text = "GROOVE";
+                arrow.GetComponent<GMArrow>().set = 1;
+                break;
+            case 2:
+                gameMode.GetComponent<Text>().text = "MOVE";
+                arrow.GetComponent<GMArrow>().set = 2;
+                break;
+            default:
+                gameMode.GetComponent<Text>().text = "DANCE";
+                arrow.GetComponent<GMArrow>().set = 0;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/LiveArrow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiveArrow : MonoBehaviour {
+
+    // Use this for initialization
+    public bool direction;
+    public GameObject gameMode;
+    public GameObject arrow;
+    public int set = 0;
+
+    public void Modify()
+    {
+        if (direction)
+        {
+            set = (set == 5) ? 5 : set+1;
+        }
+
+        else
+        {
+            set = (set == 1) ? 1 : set-1;
+        }
+
+        switch (set)
+        {
+            case 1:
+                gameMode.GetComponent<Text>().text = "1";
+                gameObject.GetComponent<Button>().interactable = false;
+                arrow.GetComponent<LiveArrow>().set = 1;
+
+                break;
+            case 2:
+                gameMode.GetComponent<Text>().text = "2";
+                arrow.GetComponent<Button>().interactable = true;
+                arrow.GetComponent<LiveArrow>().set = 2;
+                break;
+            case 3:
+                gameMode.GetComponent<Text>().text = "3";
+                arrow.GetComponent<LiveArrow>().set = 3;
+                break;
+            case 4:
+                gameMode.GetComponent<Text>().text = "4";
+                arrow.GetComponent<Button>().interactable = true;
+                arrow.GetComponent<LiveArrow>().set = 4;
+                break;
+            default:
+                gameMode.GetComponent<Text>().text = "5";
+                arrow.GetComponent<LiveArrow>().set = 5;
+                gameObject.GetComponent<Button>().interactable = false;
+                break;
+        }
+    }
+}
