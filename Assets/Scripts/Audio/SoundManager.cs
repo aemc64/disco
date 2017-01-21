90192afcfa238839c1379b477d6e50ed3c3f77a2
Zuil <zuilnorris@gmail.com>
2017-01-21 10:46:02 -0400

    Return Buttom added, soundmanager fixing

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour
 {
-	public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
-	public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+	public AudioClip[] musicSource;                 //Drag a reference to the audio source which will play the music.
+	public AudioClip AudioPlay;
 	public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
 	/*public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
 	public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
@@ -20,21 +20,39 @@ public class SoundManager : MonoBehaviour
 			instance = this;
 		//If instance already exists:
 		else if (instance != this)
-			//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-			Destroy(gameObject);
-
+			//FadeOut, this enforces our singleton pattern so there can only be one instance of SoundManager.
+			//Not working currently - Zuil
 		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
 		DontDestroyOnLoad(gameObject);
 	}
 
 
 	//Used to play single sound clips. In this case, we want the game song, menu or songlist.
-	public void PlaySingle(AudioClip clip)
+	public void PlaySingle(int clip)
 	{
 		//Set the clip of our musicSource audio source to the clip passed in as a parameter.
-		musicSource.clip = clip;
+		AudioPlay = musicSource[clip];
 
 		//Play the clip.
-		musicSource.Play();
+		//musicSource.Play;
 	}
+}
+public static class AudioFadeOut
+{
+
+	public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+	{
+		float startVolume = audioSource.volume;
+
+		while (audioSource.volume > 0)
+		{
+			audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+			yield return null;
+		}
+
+		audioSource.Stop();
+		audioSource.volume = startVolume;
+	}
+
 }
