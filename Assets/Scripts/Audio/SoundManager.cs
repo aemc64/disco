6f0a53ec16b74074a45531c50c2aeb26f21c0b73
Zuil <zuilnorris@gmail.com>
2017-01-21 12:16:02 -0400

    SoundManager Fade out now works

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class SoundManager : MonoBehaviour
 {
 	public AudioClip[] musicSource;                 //Drag a reference to the audio source which will play the music.
-	public AudioClip AudioPlay;
-	public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+	public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager. 
+	AudioSource As;
 	/*public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
 	public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
 	
@@ -21,20 +21,36 @@ public class SoundManager : MonoBehaviour
 		//If instance already exists:
 		else if (instance != this)
 			//FadeOut, this enforces our singleton pattern so there can only be one instance of SoundManager.
-			//Not working currently - Zuil
+			
 		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
 		DontDestroyOnLoad(gameObject);
+		As = gameObject.GetComponent<AudioSource>();
+		PlayWithFadeOut(Random.Range(0,1));
 	}
 
+	public void PlayWithFadeOut(int clip)
+	{
+		StartCoroutine(PlayClip(clip));
+	}
 
 	//Used to play single sound clips. In this case, we want the game song, menu or songlist.
-	public void PlaySingle(int clip)
+	public IEnumerator PlaySingle(int clip)
 	{
-		//Set the clip of our musicSource audio source to the clip passed in as a parameter.
-		AudioPlay = musicSource[clip];
-
+		//Set the clip of our musicSource audio source to the clip passed in as a parameter.	
+		As.clip = musicSource[clip];
 		//Play the clip.
-		//musicSource.Play;
+		As.Play();
+		yield return null;
+	}
+		
+
+	IEnumerator PlayClip(int clip)
+	{
+		if (As.clip != null)
+		{
+			yield return StartCoroutine(AudioFadeOut.FadeOut(As, 0.2f));
+		}
+		yield return StartCoroutine(PlaySingle(clip));
 	}
 }
 public static class AudioFadeOut
@@ -47,12 +63,13 @@ public static class AudioFadeOut
 		while (audioSource.volume > 0)
 		{
 			audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
-
 			yield return null;
+			
 		}
 
 		audioSource.Stop();
 		audioSource.volume = startVolume;
+		
 	}
 
 }
